145979db011366287e388c1786711ec1c45c8275
abil0 <hoenabil6@gmail.com>
2026-06-04 16:40:51 +0700

    try

--- Manualexceptio.cpp
+++ Manualexceptio.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-
+    try {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
