b72616a2ada6d443f1b1d56be4f9f0e078470839
abil0 <hoenabil6@gmail.com>
2026-06-04 16:10:03 +0700

    while & if

--- StreamReadersWriter.cpp
+++ StreamReadersWriter.cpp
@@ -11,5 +11,9 @@ int main () {
 
     cout << ">= menulis file,\'q\' untuk keluar" << endl;
 
-   
+    while (true) {
+        cout << "- ";
+        getline(cin,baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
     }
